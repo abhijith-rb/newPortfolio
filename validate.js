7c709e15e66fa517cd7377fff66dc73b4ee555c6
knoskimo <abhijithrb91@gmail.com>
2023-02-20 15:28:21 +0530

    m1

--- validate.js
+++ validate.js
@@ -1,50 +1,73 @@
-function validateForm(){
+function validateName(){
+    var alphs= /^[A-Za-z]+$/;
     var n= document.forms["submit-form"]["name"].value;
-    var e= document.forms["submit-form"]["email"].value;
-    var s= document.forms["submit-form"]["subject"].value;
-    var m= document.forms["submit-form"]["message"].value;
-
-        function validateName(name){
-            var alphs=/^[A-Za-z]+$/;
-            console.log(name.match(alphs))
-            return name.match(alphs);
-        }
-
-        function validateEmail(email) {
-            var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            console.log(re.test(email));
-            return re.test(email);
-        }
-
+    if(n==""){
+        document.getElementById("nameErr").innerHTML="This field is required";
+        return false;
+    }
+    else if(alphs.test(n)==false){
+        document.getElementById("nameErr").innerHTML="Please input alphabets";
+        return false;
+    }else{
+        document.getElementById("nameErr").innerHTML="";
+        return true;
+    }
     
-        if(validateName(n)==null){
-            document.getElementById("nameErr").innerHTML="Please input Alphabets only";
-        }else{
-            document.getElementById("nameErr").innerHTML="";
-        }
+}
 
-        if(!validateEmail(e)){
-            document.getElementById("emailErr").innerHTML="Enter a valid email with '@' and '.'";          
-        }else{
-            document.getElementById("emailErr").innerHTML=""; 
-        }
+function validateEmail() {
+    var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var e= document.forms["submit-form"]["email"].value;
+    console.log(re.test(e));
+    if(e==""){
+        document.getElementById("emailErr").innerHTML="This field is required";  
+        return false;        
+    }
+    else if(re.test(e)==false){
+        document.getElementById("emailErr").innerHTML="Enter a valid email with '@' and '.'";   
+        return false;        
+    }else{
+        document.getElementById("emailErr").innerHTML="";
+        return true; 
+    }
+}
 
-        if(s.length<1 || s.length>20){
-            document.getElementById("sbjErr").innerHTML="Subject should be between 1 to 20 characters";            
-        }else{
-            document.getElementById("sbjErr").innerHTML="";            
-        }
-
-        if(m.length<1 || m.length>1000){
-            document.getElementById("msgErr").innerHTML="Message should be between 1 to 1000 characters";            
-        }else{
-            document.getElementById("msgErr").innerHTML="";            
-        }
-
-        if(validateName(n)==null || !validateEmail(e) || s.length<1 || s.length>20 || m.length<1 || m.length>1000){
-            return false;
-        }else{
-            return true;
-        }   
+function validateSubject(){
+    var sub= document.forms["submit-form"]["subject"].value;
+    if(sub.length<1){
+        document.getElementById("sbjErr").innerHTML="This field is required"; 
+        return false;           
+    }else if(sub.length>20){
+        document.getElementById("sbjErr").innerHTML="Maximum 20 characters"; 
+        return false;  
+    }
+    else{
+        document.getElementById("sbjErr").innerHTML=""; 
+        return true;           
+    }
 
 }
+function validateMessage(){
+    var m= document.forms["submit-form"]["message"].value;
+    if(m.length<1){
+        document.getElementById("msgErr").innerHTML="This field is required";
+        return false;
+    }else if(m.length>1000){
+        document.getElementById("msgErr").innerHTML="Maximum 1000 characters";
+        return false;
+    }else{
+        document.getElementById("msgErr").innerHTML="";
+        return true;
+    }
+}          
+
+function validateForm(){
+
+    if(validateName()==false || validateEmail()==false || validateSubject()==false || validateMessage()==false){
+        return false;
+    }else{
+        return true;
+    }   
+}
+
+
